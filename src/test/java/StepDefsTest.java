31a80f22f319a691409f0d7f2a89878ac8e0f660
Lenar <leonruscraft@gmail.com>
2021-06-28 17:39:52 +0300

    fix: add fixed methods

--- src/test/java/StepDefsTest.java
+++ src/test/java/StepDefsTest.java
@@ -4,15 +4,27 @@ import io.cucumber.java.ParameterType;
 import io.cucumber.java.ru.И;
 import io.cucumber.java.ru.Пусть;
 import io.cucumber.java.ru.Тогда;
+import io.qameta.allure.Allure;
+import io.qameta.allure.Epic;
 import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.*;
 import typeReg.ExpensiveCategory;
 import typeReg.MainCategory;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.concurrent.TimeUnit;
 
-public class StepDefs {
+@Epic("Test avito site")
+public class StepDefsTest {
     public static LocatorPage locatorPage;
     public static WebDriver driver;
 
@@ -27,6 +39,7 @@ public class StepDefs {
     }
 
     @Before
+    @BeforeTest
     public void setup() {
         //определение пути до драйвера и его настройка
         System.setProperty("webdriver.chrome.driver", ConfProperties.getProperty("chromedriver"));
@@ -40,39 +53,50 @@ public class StepDefs {
     }
 
     @Пусть("открыт ресурс Авито")
+    @Test(priority = 1, description = "Старт приложения")
     public void getPage() {
         //получение ссылки на страницу входа из файла настроек
         driver.get(ConfProperties.getProperty("page"));
     }
 
     @И("в выпадающием списке категорий выбрана {mainCategory}")
-    public void selectCategoryOfTools(MainCategory mainCategory) {
+    @Parameters("mainCategory")
+    @Test(priority = 2, description = "в выпадающием списке категорий выбрана категория")
+    public void selectCategoryOfTools(@Optional("Оргтехника_и_расходники") MainCategory mainCategory) {
         locatorPage.selectCategoryTool(mainCategory.value);
     }
 
     @И("в поле поиска введено значение {word}")
-    public void inputFindingTools(String findStr) {
+    @Parameters("findStr")
+    @Test(priority = 3, description = "в поле поиска введено значение")
+    public void inputFindingTools(@Optional("принтер") String findStr) {
         locatorPage.inputGoods(findStr);
     }
 
     @Тогда("кликнуть по выпадающему списку региона")
+    @Test(priority = 4, description = "клик по выпадающему списку региона")
     public void clickToCityButton() {
         locatorPage.clickCityBtn();
     }
 
     @Тогда("в поле региона введено значение {word}")
-    public void inputCityAndShowAd(String city) {
+    @Parameters("city")
+    @Test(priority = 5, description = "в поле региона введено значение")
+    public void inputCityAndShowAd(@Optional("Владивосток") String city) {
         locatorPage.inputCity(city);
         locatorPage.clickBtnFindingCity(city);
     }
 
     @И("нажата кнопка показать объявления")
+    @Test(priority = 6, description = "нажата кнопка показать объявления")
     public void showAd() {
         locatorPage.clickShowAdBtn();
     }
 
     @Тогда("открыласть страница результатов по запросу {word}")
-    public void isNeedPage(String query) {
+    @Parameters("printer")
+    @Test(priority = 7, description = "открыласть страница результатов по запросу")
+    public void isNeedPage(@Optional("принтер") String query) {
         String str = driver.findElement(By.xpath("//h1[@data-marker='page-title/text']")).getText();
         if (str.contains(query)) {
             System.out.printf("open page contain %s%n", query);
@@ -82,22 +106,38 @@ public class StepDefs {
     }
 
     @И("активирован чекбокс только с доставкой")
+    @Test(priority = 8, description = "активирован чекбокс только с доставкой")
     public void checkCheckboxAndShowAd() {
         locatorPage.clickCheckBoxDelivery();
         locatorPage.clickFilterDeliverySubmitBtn();
     }
 
     @И("в выпадающем списке сортировка выбрано значение {expensiveCategory}")
-    public void filterResults(ExpensiveCategory exp) {
+    @Parameters("expensiveCategory")
+    @Test(priority = 9, description = "в выпадающем списке сортировка выбрано значение Дороже")
+    public void filterResults(@Optional("Дороже") ExpensiveCategory exp) {
         locatorPage.selectFilter(exp.value);
     }
 
     @И("в консоль выведены названия и цены {int} первых товаров")
-    public void printResults(int number) {
+    @Parameters("number")
+    @Test(priority = 10, description = "в консоль выведены названия и цены первых товаров")
+    public void printResults(@Optional("3") int number) {
         locatorPage.printTools(number);
     }
 
+    @AfterMethod
+    public void printResult() throws IOException {
+        File scrFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+
+        Path content = Paths.get(scrFile.getPath());
+        try (InputStream is = Files.newInputStream(content)) {
+            Allure.addAttachment("Screen", is);
+        }
+    }
+
     @After
+    @AfterTest
     public void quitProgram() {
         driver.quit();
     }
