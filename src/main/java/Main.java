c6e821f25e7bde3743b486171efbcd778bed9fa1
Lenar <leonruscraft@gmail.com>
2021-06-28 14:25:39 +0300

    fix: locator problems 2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,64 +1,23 @@
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.Select;
-import org.openqa.selenium.support.ui.WebDriverWait;
-
-import java.time.Duration;
-import java.util.List;
-
 public class Main {
     public static void main(String[] args) {
-        // Драйвер для версии Google Chrome 91.0.4472.114
-        System.setProperty("webdriver.chrome.driver", "src\\main\\resources\\chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
-
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-        driver.manage().window().maximize();
-
-        driver.get("https://www.avito.ru/");
-
+        ManipulateSite site = new ManipulateSite();
+        // точка входа
+        site.setup();
         // Выберем в выпадающем списке “категория”  значение оргтехника и расходники
-        Select selectCategory = new Select(driver.findElement(By.xpath("//select[@name='category_id']")));
-        selectCategory.selectByVisibleText("Оргтехника и расходники");
-
+        site.selectCategoryOfTools();
         // В поле поиск по объявлению введем значение “Принтер”
-        WebElement findTool = driver.findElement(By.xpath("//input[@data-marker='search-form/suggest']"));
-        findTool.sendKeys("Принтер");
-
+        site.inputFindingTools();
         // Нажмем на поле город
-        WebElement cityButton = driver.findElement(By.xpath("//div[@data-marker='search-form/region']"));
-        cityButton.click();
-
+        site.clickToCityButton();
         // Заполним значением “Владивосток” поле город  в открывшемся окне и кликнем по первому предложенному варианту
         // Нажмем на кнопку “Показать объявления”
-        WebElement findCity = driver.findElement(By.xpath("//input[@data-marker='popup-location/region/input']"));
-        String city = "Владивосток";
-        findCity.sendKeys(city);
-        
-        drv.findElement(By.xpath("//li[@data-marker='suggest(0)']")).click();
-        drv.findElement(By.xpath("//button[@data-marker='popup-location/save-button']")).click();
-
+        site.inputCityAndShowAd();
         // Проверим, активирован ли чекбокс, и если не активирован – активируем и нажмем кнопку “Показать объявления”
-        WebElement checkBox = driver.findElement(By.xpath("//div[@data-marker='delivery-filter/container']"));
-        if (!checkBox.isSelected()) {
-            checkBox.click();
-        }
-
-        driver.findElement(By.xpath("//button[@data-marker='search-filters/submit-button']")).click();
-
-        // В выпадающем списке фильтрации выберем фильтрацию по убыванию цены.
-        Select selectFilter = new Select(driver.findElement(By.xpath("//div[@class='sort-select-3QxXG select-select-box-3LBfK select-size-s-2gvAy']/select")));
-        selectFilter.selectByVisibleText("Дороже");
-
+        site.checkCheckboxAndShowAd();
+        // В выпадающем списке фильтрации выберем фильтрацию по убыванию цены
+        site.filterResults();
         // Выведем в консоль название и стоимость 3х самых дорогих принтеров
-        List<WebElement> printers = driver.findElements(By.xpath("//div[@data-marker='catalog-serp']/div[@data-marker='item']"));
-        for (int i = 0; i < 3; i++) {
-            System.out.println(printers.get(i).findElement(By.xpath(".//h3[@itemprop='name']")).getText() +
-                                " : " + printers.get(i).findElement(By.xpath(".//meta[@itemprop='price']")).getAttribute("content") + " руб.");
-        }
+        site.printResults();
 
         // Делаю принудительное ожидание чтобы успеть увидеть работу программы
         try {
@@ -66,7 +25,7 @@ public class Main {
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-
-        driver.quit();
+        // закрываем и останавливаем драйвер
+        site.quitProgram();
     }
 }
---
+++ src/main/java/ManipulateSite.java
@@ -0,0 +1,65 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import java.time.Duration;
+
+public class ManipulateSite {
+    public static LocatorPage locatorPage;
+    public static WebDriver driver;
+
+    public void setup() {
+        //определение пути до драйвера и его настройка
+        System.setProperty("webdriver.chrome.driver", ConfProperties.getProperty("chromedriver"));
+        //создание экземпляра драйвера
+        driver = new ChromeDriver();
+        locatorPage = new LocatorPage(driver);
+        //окно разворачивается на полный экран
+        driver.manage().window().maximize();
+        //задержка на выполнение теста = 10 сек.
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        //получение ссылки на страницу входа из файла настроек
+        driver.get(ConfProperties.getProperty("page"));
+    }
+
+    // Выберем в выпадающем списке “категория”  значение оргтехника и расходники
+    public void selectCategoryOfTools() {
+        locatorPage.selectCategoryTool("Оргтехника и расходники");
+    }
+
+    // В поле поиск по объявлению введем значение “Принтер”
+    public void inputFindingTools() {
+        locatorPage.inputGoods("принтер");
+    }
+
+    // Нажмем на поле город
+    public void clickToCityButton() {
+        locatorPage.clickCityBtn();
+    }
+
+    // Заполним значением “Владивосток” поле город  в открывшемся окне и кликнем по первому предложенному варианту
+    // Нажмем на кнопку “Показать объявления”
+    public void inputCityAndShowAd() {
+        locatorPage.inputCity("Владивосток");
+        locatorPage.clickBtnFindingCity("Владивосток");
+        locatorPage.clickShowAdBtn();
+    }
+
+    // Проверим, активирован ли чекбокс, и если не активирован – активируем и нажмем кнопку “Показать объявления”
+    public void checkCheckboxAndShowAd() {
+        locatorPage.clickCheckBoxDelivery();
+        locatorPage.clickFilterDeliverySubmitBtn();
+    }
+
+    // В выпадающем списке фильтрации выберем фильтрацию по убыванию цены.
+    public void filterResults() {
+        locatorPage.selectFilter("Дороже");
+    }
+
+    // Выведем в консоль название и стоимость 3х самых дорогих принтеров
+    public void printResults() {
+        locatorPage.printTools(3);
+    }
+
+    public void quitProgram() {
+        driver.quit();
+    }
+}
